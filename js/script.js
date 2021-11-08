df86d864602b95695d7ba50d4747c926e86c8a4c
Lumi Cassandra <lumiru@protonmail.com>
2021-11-08 17:29:04 -0300

    Implemented game function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ function computerPlay() {
     let choice = Math.random(1, 3) * (3 - 1) + 1    
     return options[choice]
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toUpperCase()
+    computerSelection.toUpperCase()
+    
+}
+
+function game(numOfRounds) {
+    for (let i = 0; i < numOfRounds; i++) {
+        playRound(playerSelection, computerSelection)
+    }
+}
+
+const numOfRounds = 5;
+const playerSelection = prompt("Pick your tool: ");
+const computerSelection = computerPlay();
